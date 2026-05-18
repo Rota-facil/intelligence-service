d11aa36b840ddb45cab72791bd3ffe948a0ebdf7
talvan15 <talvan.alves00@gmail.com>
2026-05-18 14:43:39 -0300

    infra: connect to eureka-server

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
-from http_routes.controllers.intelligence_controller import router as router_intelligence
+import os
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+import py_eureka_client.eureka_client as eureka
+
+from fastapi import FastAPI
+
+from http_routes.controllers.intelligence_controller import (
+    router as router_intelligence
+)
+
+EUREKA_SERVER = os.environ.get(
+    "EUREKA_SERVER",
+    "http://localhost:8081/eureka/"
+)
+
+INTELLIGENCE_HOST = os.environ.get(
+    "INTELLIGENCE_HOST",
+    "localhost"
+)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    await eureka.init_async(
+        eureka_server=EUREKA_SERVER,
+        app_name="intelligence-service",
+        instance_port=8000,
+        instance_host=INTELLIGENCE_HOST
+    )
+
+    yield
+
+    await eureka.stop()
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(router_intelligence)
+
 
 @app.get("/")
 async def root():
